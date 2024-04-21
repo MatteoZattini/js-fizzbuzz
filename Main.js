6f9438c159cf5c1431c114fe187c8698b3a772b1
MatteoZattini <matteozattini@hotmail.com>
2024-04-21 11:27:20 +0200

    added bonus 1

--- Main.js
+++ Main.js
@@ -1,14 +1,26 @@
-for (let i = 1; i <= 100; i++) {
+const myContainer = document.querySelector('div')
 
+for (let i = 1; i <= 100; i++) {
+    
+    if (i % i == 0 && i % 3 != 0 && i % 5 != 0 ) {
+        console.log(i);
+        const myItem = document.createElement('span')
+        myContainer.append(myItem)
+        myItem.innerHTML = ` ${i}`
+    }
     if (i % 3 == 0) {
         console.log("fizz");
+        const myItem = document.createElement('span')
+        myContainer.append(myItem)
+        myItem.innerHTML = ` ${"fizz"}`
     }
     if (i % 5 == 0) {
         console.log("buzz");
+        const myItem = document.createElement('span')
+        myContainer.append(myItem)
+        myItem.innerHTML = "buzz"
     }
-    if (i % i == 0 && i % 3 != 0 && i % 5 != 0 ) {
-        console.log(i);
-    }
+
 }
 
 
